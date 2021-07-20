a3a7ba97a79c450ecdc6132be95fdbccb9506778
amalcaraz <amalcaraz89@gmail.com>
2021-07-20 17:05:25 +0200

    added configuration files

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
+const { config } = require('dotenv')
 const { startApi } = require('./src/api')
+
+config()
 startApi()
--- src/controller/health.js
+++ src/controller/health.js
@@ -1,5 +1,10 @@
+const config = require('config')
+
 function healthController(req, res, next) {
-  res.status(200).json({ alive: true })
+  res.status(200).json({
+    id: config.get('id'),
+    alive: true
+  })
 }
 
 module.exports = healthController
